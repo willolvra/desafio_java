7c3e1baa4a4b05d367995079e200c7df17d8de42
willolvra <williamolaz@outlook.com>
2025-03-12 07:42:55 -0300

    Começo do desafio, criação das classes

---
+++ src/entities/Order.java
@@ -0,0 +1,73 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client;
+	private List<OrderItem> orderItem = new ArrayList<>();;
+	
+	public Order() {
+		
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+	
+	public void addItem(OrderItem item) {
+		orderItem.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		orderItem.remove(item);
+	}
+	
+	public double total() {
+		double sum = 0;
+		for (OrderItem c : orderItem) {
+			sum += c.subTotal();
+		}
+		return sum;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getOrderitem() {
+		return orderItem;
+	}
+
+	public void setOrderitem(List<OrderItem> orderitem) {
+		this.orderItem = orderitem;
+	}
+}
